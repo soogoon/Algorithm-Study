211d5a19002f0256b6317f90a6397dffcdda5f89
조윤영 <yoon980208@gmail.com>
2020-06-22 18:09:10 +0900

    [UPDATE]리드미 업로드 초기화

--- yoonyoung/DrawingDoll/DrawingDoll/main.cpp
+++ yoonyoung/DrawingDoll/DrawingDoll/main.cpp
@@ -8,17 +8,42 @@
 
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
+bool visited[6][6];
+
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
+    stack<int> s;
     
+    for(int i=0; i<moves.size(); i++) {
+        int index = moves[i]-1;
+        
+        for(int j=0; j<board[index].size(); j++) {
+            if(board[index][j] !=0 && !visited[index][j]) {
+                cout<<board[index][j]<<endl;
+                visited[index][j] = true;
+                if(!s.empty() && s.top() == board[index][j]){
+                    s.pop();
+                    answer += 2;
+                }else {
+                    s.push(board[index][j]);
+                }
+                break;
+            }
+            
+        }
+    }
+    
+    
+    return answer;
 }
 
 int main(int argc, const char * argv[]) {
     vector<vector<int>> board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
     vector<int> moves = {1,5,3,5,1,2,1,4};
-    solution(board, moves);
+    cout<<solution(board, moves);
     return 0;
 }
