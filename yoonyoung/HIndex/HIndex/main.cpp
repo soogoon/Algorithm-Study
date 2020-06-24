9c2d5f8144db297edc0339419e56d8064a3354a2
조윤영 <yoon980208@gmail.com>
2020-06-24 15:12:38 +0900

    [UPDATE] AC 풀이 업로드

---
+++ yoonyoung/HIndex/HIndex/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  HIndex
+//
+//  Created by 조윤영 on 2020/06/23.
+//  Copyright © 2020 조윤영. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void solution() {
+    
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
