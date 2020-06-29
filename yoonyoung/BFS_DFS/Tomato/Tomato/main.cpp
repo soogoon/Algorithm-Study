d66816753dd9ed085f0f839650302d1788bed572
조윤영 <yoon980208@gmail.com>
2020-06-29 23:58:09 +0900

    [UPDATE] 토마토 업로드

--- yoonyoung/BFS_DFS/Tomato/Tomato/main.cpp
+++ yoonyoung/BFS_DFS/Tomato/Tomato/main.cpp
@@ -18,7 +18,6 @@ int M = 0;
 int N = 0;
 int H = 0;
 int room[MAX_SIZE][MAX_SIZE][MAX_SIZE];
-int visited[MAX_SIZE][MAX_SIZE];
 
 struct Dir {
     int h;
@@ -38,6 +37,8 @@ struct Tomato {
     };
 };
 queue<Tomato> ripen_tomato;
+int tomatoSum = 0;
+int ripenTomatoSum = 0;
 
 void input() {
     cin>> M >> N >> H;
@@ -46,35 +47,64 @@ void input() {
         for(int i=0; i<N;  i++) {
             for(int j=0; j<M; j++) {
                 cin>> room[h][i][j];
-                if(room[h][i][j] == 1) ripen_tomato.push(Tomato(h, i, j));
+                if(room[h][i][j] == 1) {
+                    ripen_tomato.push(Tomato(h, i, j));
+                    tomatoSum++;
+                    ripenTomatoSum++;
+                }else if (room[h][i][j] == 0) tomatoSum++;
             }
         }
     }
 }
 
-void solution() {
+bool isInBoundary(int box, int y, int x) {
+    if(0<= y && y<N && 0<=x && x<M && 0<=box && box<H) return true;
+    return false;
+}
+
+int solution() {
+    int answer = 0;
+    
     while(!ripen_tomato.empty()) {
-        int tomatoBox = ripen_tomato.front().box;
-        int tomatoY = ripen_tomato.front().y;
-        int tomatoX = ripen_tomato.front().x;
+        bool isRipen = false;
+        int tomatoCnt = ripen_tomato.size();
         
-        for(int i=0; i<6; i++) {
-            int nextTomatoBox = tomatoBox + moveDir[i].h;
-            int nextTomatoY = tomatoBox + moveDir[i].h;
-            int nextTomatoX = tomatoBox + moveDir[i].h;
+        
+        for(int i=0; i<tomatoCnt; i++) {
+            int tomatoBox = ripen_tomato.front().box;
+            int tomatoY = ripen_tomato.front().y;
+            int tomatoX = ripen_tomato.front().x;
+            ripen_tomato.pop();
             
-            if(0<=nextTomatoBox && nextTomatoBox<h) {
+            for(int i=0; i<6; i++) {
+                int nextTomatoBox = tomatoBox + moveDir[i].h;
+                int nextTomatoY = tomatoY + moveDir[i].y;
+                int nextTomatoX = tomatoX + moveDir[i].x;
                 
+                if(isInBoundary(nextTomatoBox, nextTomatoY, nextTomatoX)) {
+                    int nextTomato = room[nextTomatoBox][nextTomatoY][nextTomatoX];
+                    
+                    if (nextTomato == 0) {//익지 않은 토마토라면,
+                         isRipen = true;
+                        room[nextTomatoBox][nextTomatoY][nextTomatoX] = 1;
+                        ripen_tomato.push(Tomato(nextTomatoBox, nextTomatoY, nextTomatoX));
+                        ripenTomatoSum++;
+                    }
+                }
             }
-            
         }
+        if(isRipen)answer++;
     }
+    
+    if(ripenTomatoSum < tomatoSum) answer = -1;
+    
+    return answer;
 }
 
 int main(int argc, const char * argv[]) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     input();
-    solution();
+    cout<<solution()<<"\n";
     return 0;
 }
